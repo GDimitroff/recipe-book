ad0ce5fac9594d62db1219ac91bc0f9fb6271225
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-07-23 14:21:30 +0300

    Display recipe list and detail components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a simply a test',
+            'https://bestofvegan.com/wp-content/uploads/2020/10/Pumpkin-Samosas-Samsa-with-Cilantro-Mint-Chutney_01.jpg'),
+        new Recipe('A Test Recipe', 'This is a simply a test',
+            'https://bestofvegan.com/wp-content/uploads/2020/10/Pumpkin-Samosas-Samsa-with-Cilantro-Mint-Chutney_01.jpg')
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
