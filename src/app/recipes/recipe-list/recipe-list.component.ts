ddf0ff299e286627d6f0ad14de8adcdcb52920ff
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-07-24 14:15:31 +0300

    Implementing detail page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,22 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a simply a test',
+        new Recipe('A Test Recipe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a elit id justo sagittis luctus eu vitae dui.',
             'https://bestofvegan.com/wp-content/uploads/2020/10/Pumpkin-Samosas-Samsa-with-Cilantro-Mint-Chutney_01.jpg'),
-        new Recipe('A Test Recipe', 'This is a simply a test',
-            'https://bestofvegan.com/wp-content/uploads/2020/10/Pumpkin-Samosas-Samsa-with-Cilantro-Mint-Chutney_01.jpg')
+        new Recipe('Test Number 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a elit id justo sagittis luctus eu vitae dui.',
+            'https://img.taste.com.au/vhZfDaJw/taste/2017/07/lemongrass-beef-bowl-128353-1.jpg')
     ];
 
     constructor() { }
 
     ngOnInit(): void {
+    }
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
     }
 
 }
