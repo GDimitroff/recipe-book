ff1f4057b8f0f50004c12726c37c256c778dcfe2
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-07-26 14:54:39 +0300

    Implemented basic shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,25 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a elit id justo sagittis luctus eu vitae dui.',
-            'https://bestofvegan.com/wp-content/uploads/2020/10/Pumpkin-Samosas-Samsa-with-Cilantro-Mint-Chutney_01.jpg'),
-        new Recipe('Test Number 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a elit id justo sagittis luctus eu vitae dui.',
-            'https://img.taste.com.au/vhZfDaJw/taste/2017/07/lemongrass-beef-bowl-128353-1.jpg')
+        new Recipe('Tasty Schnitzel', 'A super tasy Schnitzel - just awesome!',
+            'https://upload.wikimedia.org/wikipedia/commons/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Big Fat Burger', 'What else you need to say?',
+            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
 
     getRecipes(): Recipe[] {
