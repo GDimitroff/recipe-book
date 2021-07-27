ceb60a073350cdf320eafbbb053b7dde810d8d51
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2021-07-27 17:13:11 +0300

    Basic routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/recipes'
+    },
+    {
+        path: 'recipes',
+        component: RecipesComponent,
+        children: [
+            {
+                path: '',
+                component: RecipeStartComponent
+            },
+            {
+                path: ':id',
+                component: RecipeDetailComponent
+            }
+        ]
+    },
+    {
+        path: 'shopping-list',
+        component: ShoppingListComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
     selector: 'app-recipe-item',
@@ -11,12 +10,6 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
     @Input() recipe: Recipe;
 
-    constructor(private recipeServece: RecipeService) { }
-
     ngOnInit(): void {
-    }
-
-    onSelected() {
-        this.recipeServece.recipeSelected.emit(this.recipe);
     }
 }
